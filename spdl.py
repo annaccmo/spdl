9baf4d34eba47a0a77a424e7b1a0e8300128eece
annaccmo <cmoliveira.ac@gmail.com>
2021-03-30 19:09:07 -0300

    Update spdl.py

--- spdl.py
+++ spdl.py
@@ -8,21 +8,22 @@ def modelCheck (initialState, finalState, actions):
     if finalState != initialState:
         actual = finalState
         
-        difStates = (finalState - initialState) | (initialState - finalState)
+        #difStates = (finalState - initialState) | (initialState - finalState)
 
     statesStack = []
+    actionStack = []
     i = 0
     while True: #Pensar numa condição de parada
         for a in actions:
-
             if a['add'] <= actual and ((a['del'] & actual) == set()):
-                newState = (actual - a['add']) | a['del']
+                newState = (actual - a['add']) | a['del'] 
                 if a['pre'] <= newState and not(newState in statesStack):
-                    difNew = (newState - initialState) | (initialState - newState)
-                    if difStates > difNew:
-                        actual = newState
-                        if (newState <= initialState) or (initialState <= newState):#faz sentido essa condição?
-                            return statesStack
+                    #difNew = (newState - initialState) | (initialState - newState)
+                    #if difStates > difNew:
+                    actual = newState
+                    actionStack.append(a)
+                    if (newState <= initialState) or (initialState <= newState):#faz sentido essa condição?
+                        return statesStack, actionStack
                     i=0
                 else:
                     i+=1
